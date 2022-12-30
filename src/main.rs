50e183f7ddb7df1e7ca35769b5c3dc9d5fa5bc04
LiXuanqi <lixuanqi1995@gmail.com>
2022-12-29 22:21:07 -0800

    handle concurrent clients

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // Uncomment this block to pass the first stage
 use std::io::{Read, Write};
 use std::net::TcpListener;
+use std::thread;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,22 +12,24 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
     for stream in listener.incoming() {
-        match stream {
-            Ok(mut _stream) => {
-                loop {
-                    let mut buffer = [0; 512];
-                    _stream.read(&mut buffer).unwrap();
+        thread::spawn(move || {
+            match stream {
+                Ok(mut _stream) => {
+                    loop {
+                        let mut buffer = [0; 512];
+                        _stream.read(&mut buffer).unwrap();
 
-                    // let result = String::from_utf8_lossy(&buffer[..]);
-                    // println!("{}", result);
-                    
-                    _stream.write("+PONG\r\n".as_bytes()).unwrap();
-                    _stream.flush().unwrap();
+                        // let result = String::from_utf8_lossy(&buffer[..]);
+                        // println!("{}", result);
+                        
+                        _stream.write("+PONG\r\n".as_bytes()).unwrap();
+                        _stream.flush().unwrap();
+                    }
+                }
+                Err(e) => {
+                    println!("error: {}", e);
                 }
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        });
     }
 }
